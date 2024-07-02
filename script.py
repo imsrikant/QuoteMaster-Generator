959e9915ff02279fdfc0ca6b140fdaa27b4ed289
Srikant Kumar <52560067+imsrikant@users.noreply.github.com>
2024-07-02 04:34:18 +0000

    chore: Refactor script.py to handle command line arguments

--- script.py
+++ script.py
@@ -2,3 +2,18 @@ from sys import argv, exit
 import requests
 
 API = 'https://api.kanye.rest'
+
+def main():
+    ...
+
+def check_argv(argv):
+    if len(argv) > 2:
+        exit("Pass only one argument")
+
+    if len(argv) == 1:
+        return 1
+
+    if not argv[1].isdigit():
+        exit("Enter number to print that many quotes")
+
+    return int(argv[1])
